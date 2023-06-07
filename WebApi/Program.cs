0153ec0bcc58ce72e63e48346aca3f4bb2e37077
gustavoalmeid4 <almades159@gmail.com>
2023-06-07 01:02:36 -0300

    adicionando Singleton

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,9 @@
+using Domain.Interfaces.Generics;
+using Entities.Entidades;
+using Infra.Configuracao;
+using Infra.Repositorio;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +14,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<ContextBase>(options =>
+               options.UseSqlServer(
+                   builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ContextBase>();
+
+builder.Services.AddSingleton(typeof(InterfaceGeneric<>), typeof(RepositoryGenerics<>));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
